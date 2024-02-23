792e067f3ea176eeb7ab006dae72a3370e21a1f4
arunarajendran2014 <rajendran.aruna2014@gmail.com>
2024-02-23 16:08:46 +0530

    Refactored test method for divisible of 400

--- leap-year/src/test/java/com/bnpp/leap_year/LeapYearKataTest.java
+++ leap-year/src/test/java/com/bnpp/leap_year/LeapYearKataTest.java
@@ -1,5 +1,7 @@
 package com.bnpp.leap_year;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class LeapYearKataTest {
@@ -7,8 +9,9 @@ public class LeapYearKataTest {
 	@Test
 	public void return0DivisibleOf400() {
 		LeapYearKataService leapYr = new LeapYearKataService();
-		int input = 1;
-		leapYr.getResultOfLeapYear(input);
+		int input = 0;
+		int expected = 0;
+		assertEquals(expected,leapYr.getResultOfLeapYear(input));
 	}
 	
 }
