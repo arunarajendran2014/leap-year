01a4d7434d4225b851706d1c54f406da492c264d
arunarajendran2014 <rajendran.aruna2014@gmail.com>
2024-02-23 16:30:29 +0530

    Refactored service method variables

--- leap-year/src/main/java/com/bnpp/leap_year/LeapYearKataService.java
+++ leap-year/src/main/java/com/bnpp/leap_year/LeapYearKataService.java
@@ -3,15 +3,12 @@ package com.bnpp.leap_year;
 public class LeapYearKataService {
 
 	public int getResultOfLeapYear(int input) {
-		
+		int result = input;
 		if (input % 400 == 0)
 		{
-		
-		return 0;
+		result = 0;
 		}
-		return input;
-		
-		
+		return result;
 	}
 
 }
